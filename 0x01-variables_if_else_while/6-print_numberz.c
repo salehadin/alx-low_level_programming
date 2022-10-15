a54d53f2837ae7fd481142de37856d686a782e59
salehadin <salanur5656@gmail.com>
2022-10-15 02:22:50 -0700

    single digit in base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints a single digit in base 10
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
