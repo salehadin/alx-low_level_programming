690564600975cfda3bb0b29bb055954d09dfad6e
salehadin <salanur5656@gmail.com>
2022-11-01 12:26:19 -0700

    main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,7 @@
-#ifndef
-#define
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -8,5 +10,6 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
-int _putchar(char c);
-#endif
+void set_string(char **s, char *to);
+
+#endif /* MAIN_H */
