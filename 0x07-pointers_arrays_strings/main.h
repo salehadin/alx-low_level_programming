06b298ade2f38b2d48442f0d35a564b61e673d72
salehadin <salanur5656@gmail.com>
2022-11-01 13:07:03 -0700

    advanced 100

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -11,5 +11,5 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-
+void set_string(char **s, char *to);
 #endif /* MAIN_H */
