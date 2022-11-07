652a8f874b46ce9bf6d6902aa2845239437d630b
salehadin <salanur5656@gmail.com>
2022-11-07 13:23:41 -0800

    argstostr

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,50 +1,48 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "main.h"
 /**
- * *argstostr - convert arguments on command line to strings
- * @ac: int type
- * @av: pointer to array
- * Return: arguments as strings
- */
+*size - obtain the size of an array of 2 dimensions
+*@c: number of arguments
+*@v: array of pointers to strings
+*Return: size of an array
+*/
+int size(int c, char **v)
+{
+	int rec1, rec2, sz = 0;
 
+	for (rec1 = 0; rec1 < c; rec1++)
+	{
+		for (rec2 = 0; v[rec1][rec2]; rec2++)
+			sz++;
+		sz++;/*space for newline*/
+	}
+	sz++;/*space for string terminator*/
+	return (sz);
+}
+/**
+*argstostr - concatenates all the arguments of your program
+*@ac: number of arguments
+*@av: array of pointers to strings
+*Return: pointer to a new string or NULL
+*/
 char *argstostr(int ac, char **av)
 {
-	int size, count, count1, count2 = 0;
-	char *ptr;
+	char *a;
+	int i, j, k, len = 0;
 
-	if (ac == 0 || av == NULL)
-	{
+	if (ac == 0 || av == 0)
 		return (NULL);
-	}
-
-	for (count = 0; count < ac; count++)
-	{
-		for (count1 = 0; av[count][count1] != '\0'; count1++)
-		{
-			size += 1;
-		}
-		size += 1;
-	}
-	size += 1;
-
-	ptr = malloc(sizeof(char) * size);
-	if (ptr == NULL)
-	{
-		free(ptr);
+	len = size(ac, av);
+	a = (char *) malloc(len * sizeof(char));
+	if (a == NULL)
 		return (NULL);
-	}
-	for (count = 0; count < ac; count++)
+	for (i = 0, k = 0; i < ac; ++i, ++k)
 	{
-		for (count1 = 0; av[count][count1] != '\0'; count1++)
-		{
-			ptr[count2] = av[count][count1];
-			count2++;
-		}
-		ptr[count2] = '\n';
-		count2++;
+		for (j = 0; av[i][j]; ++j, ++k)
+			a[k] = av[i][j];
+		a[k] = '\n';
 	}
-	ptr[count2] = '\0';
-	return (ptr);
+	a[k + 1] = '\0';
+	return (a);
 }
