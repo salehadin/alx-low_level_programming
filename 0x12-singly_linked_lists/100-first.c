dc61d5530ec94076ee264b48b482ae2f43354f5b
salehadin <salanur5656@gmail.com>
2022-11-25 10:50:14 -0800

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * tortoise -  before mian.
+ *
+ * Return: nothing
+ */
+void tortoise(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
